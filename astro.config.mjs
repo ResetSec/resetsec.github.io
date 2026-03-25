5121ff2892ae771e56355dd9aa7546fe2afdcf8d
VulnX <vulnx101@gmail.com>
2026-03-25 18:40:03 +0530

    nuke

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({
-	site: 'https://vulnx.github.io',
-	base: '/resetsec.github.io'
-});
