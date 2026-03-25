c84f4e8b71ce51953249203b8907e11a6ebd2c9f
VulnX <vulnx101@gmail.com>
2026-03-25 18:47:16 +0530

    hello astro again

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// NOTE: Revert this to default during merge.
+export default defineConfig({
+	site: 'https://vulnx.github.io',
+	base: '/resetsec.github.io'
+});
