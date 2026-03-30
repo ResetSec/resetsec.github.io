2e6f288a01baacc304b1159ffdeffd93c3f58202
Krishna Chomal <62636727+VulnX@users.noreply.github.com>
2026-03-30 17:11:59 +0530

    Clear site and base settings in astro.config.mjs
    Removed site and base configuration from astro.config.mjs.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // NOTE: Revert this to default during merge.
-export default defineConfig({
-	site: 'https://vulnx.github.io',
-	base: '/resetsec.github.io'
-});
+export default defineConfig({});
