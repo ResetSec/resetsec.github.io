ba958a09a2ebd4e0e0eec4150a97fda651dbd8b8
VulnX <vulnx101@gmail.com>
2026-03-30 18:10:49 +0530

    fix: use base in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // NOTE: Revert this to default during merge.
 export default defineConfig({
-	site: 'https://resetsec.xyz'
+	site: 'https://resetsec.xyz',
+	base: '/'
 });
